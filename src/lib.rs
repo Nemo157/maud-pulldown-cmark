41f52a8b8a400572c7c161ef6c2310bd3fa55721
Wim Looman <wim@nemo157.com>
2016-02-09 20:25:21 +0100

    Enable most builtin warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 //! This library implements an adapter to allow rendering strings as markdown inside a `maud`
 //! macro using `pulldown-cmark` efficiently.
 
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unsafe_code)]
+#![warn(unused_extern_crates)]
+#![warn(unused_qualifications)]
+#![warn(unused_results)]
 #![warn(variant_size_differences)]
 
 extern crate maud;
